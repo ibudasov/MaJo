1a7451943ce9d26eb775fee37719ebb2300a9f1a
Igor Budasov <igor.budasov@gmail.com>
2015-10-15 21:51:46 +0200

    Initial Commit

---
+++ MaJo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MaJo
+//
+//  Created by Igor Budasov on 15.10.15.
+//  Copyright (c) 2015 Igor Budasov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
